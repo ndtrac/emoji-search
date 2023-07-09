7cb705286d20bf278a64e651204e5ae6c0586e8b
Nguyen Duy Trac <ndtrac@gmail.com>
2023-07-09 21:29:28 +0700

    update spec

--- cypress/e2e/sample_spec.cy.js
+++ cypress/e2e/sample_spec.cy.js
@@ -1,5 +1,5 @@
 describe('Sample test', () => {
-    it('Search lollipop emoji', () => {
+    it('Search lollipop emoji test', () => {
         cy.visit('http://localhost:3000/') // launch project website
         cy.get('input').type('lollipop') // enter 'lollipop' in search 
         cy.get('.component-emoji-result-row').should('have.length', 1) // check if only 1 result is returned 
