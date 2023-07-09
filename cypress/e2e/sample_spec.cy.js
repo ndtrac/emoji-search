6b036600a0a7d235ab5a523eb79c6d20fc48e687
ndtrac <34302444+ndtrac@users.noreply.github.com>
2023-07-09 17:26:54 +0700

    Add files via upload

---
+++ cypress/e2e/sample_spec.cy.js
@@ -0,0 +1,8 @@
+describe('Sample test', () => {
+    it('Search lollipop emoji', () => {
+        cy.visit('http://localhost:3000/') // launch project website
+        cy.get('input').type('lollipop') // enter 'lollipop' in search 
+        cy.get('.component-emoji-result-row').should('have.length', 1) // check if only 1 result is returned 
+        cy.contains('Lollipop').should('have.class', 'title') // check if returned element contains 'title' class
+    })
+})
